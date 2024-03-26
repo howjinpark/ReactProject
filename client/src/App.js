03e29060112480a1673a90d49c77a3f5db96b381
howjinpark <pho30@naver.com>
2024-03-26 13:48:00 +0900

    Add Custoemr API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,22 @@ import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
+import CircularProgress from '@mui/material/CircularProgress';
+import { withStyles } from '@mui/material/styles';
+
+const styles =theme=>({
+  root: {
+    width: '100%' ,
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit *2
+  }
+});
 
 
 
@@ -13,10 +29,12 @@ import TableCell from '@mui/material/TableCell';
 class App extends Component{
 
   state = {
-    customers:""
+    customers:"",
+    completed: 0
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err=>console.log(err));
@@ -26,6 +44,11 @@ class App extends Component{
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
+  }
+
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed + 1});
   }
 
   render(){
@@ -57,7 +80,14 @@ class App extends Component{
             />
           )
         })
-      : ""}
+      : 
+      <TableRow>
+        <TableCell colSpan="6" align="center">
+        <CircularProgress className={styles.progress} variant="determinate" value={this.state.completed}/>
+
+        </TableCell>
+      </TableRow>
+      }
         </TableBody>
         </Table>
       </div>
